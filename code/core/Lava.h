c5c76b42c30fb89f640a70251e537b73e9aaa92d
Satoshi-PC <satoshi.yoda.register.264@gmail.com>
2020-12-31 20:38:05 +0300

    Removed old code.

--- code/core/Lava.h
+++ code/core/Lava.h
@@ -18,53 +18,8 @@
 #include "Mountain.h"
 #include "Rocks.h"
 
-using std::array;
-using std::mutex;
 using std::unique_ptr;
 using std::vector;
-using glm::vec2;
-using glm::vec3;
-
-// TODO
-// Cave
-// store batch all data in 1 class with state
-// make establish(), freeCPU(), freeGPU(), etc methods
-// store lavaObjectId (just index), stats in that class
-// aspects: working, staging, live
-
-struct BatchCreateData {
-	vector<Vertex> vertices;
-	vector<Instance> instances;
-	int width;
-	int height;
-	void* pixels;
-	vector<size_t> free;
-};
-
-struct BatchLiveData {
-	VkBuffer vertexBuffer;
-	VmaAllocation vertexAllocation;
-	VmaAllocationInfo vertexInfo;
-	uint32_t vertexCount;
-	VkBuffer stagingVertexBuffer;
-	VmaAllocation stagingVertexAllocation;
-	VmaAllocationInfo stagingVertexInfo;
-
-	VkBuffer instanceBuffer;
-	VmaAllocation instanceAllocation;
-	VmaAllocationInfo instanceInfo;
-	uint32_t instanceCount;
-	VkBuffer stagingInstanceBuffer;
-	VmaAllocation stagingInstanceAllocation;
-	VmaAllocationInfo stagingInstanceInfo;
-
-	VkImage textureImage;
-	VmaAllocation textureAllocation;
-	VkImageView textureView;
-	VkBuffer stagingTextureBuffer;
-	VmaAllocation stagingTextureAllocation;
-	VmaAllocationInfo stagingTextureInfo;
-};
 
 class Lava {
 public:
