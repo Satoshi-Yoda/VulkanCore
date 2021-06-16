2cb170a8b0b76d359e7498925e9fef397c8ba18b
Satoshi-PC <satoshi.yoda.register.264@gmail.com>
2021-06-16 03:06:24 +0300

    todo

--- code/core/Tectonic.cpp
+++ code/core/Tectonic.cpp
@@ -92,11 +92,6 @@ void Tectonic::prepareFrame(uint32_t craterIndex) {
 		// vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0, 0, nullptr, 0, nullptr, 1, &barrierFromPresentToDraw);
 		// vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, 0, 0, nullptr, 0, nullptr, 1, &barrierFromPresentToDraw);
 
-		// So, for second drawable I need:
-		// - all from cave or similar
-	 	// - pipeline
-		// - pipelineLayout
-
 		VkRenderPassBeginInfo passBeginInfo { VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO };
 		passBeginInfo.renderPass = crater.renderPass;
 		passBeginInfo.framebuffer = crater.framebuffers[craterIndex];
